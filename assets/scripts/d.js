9effad17df3af31700ca8dca94c8dc57d1799871
070ry <onewsacount@gmail.com>
2023-08-16 10:28:49 +0900

    v1.3

--- assets/scripts/d.js
+++ assets/scripts/d.js
@@ -1,12 +1,4 @@
-(function () {
-  var image = new Image();
-  Object.defineProperty(image, "id", {
-    get() {
-      image = null;
-      location.href = "../../devtools.html";
-    },
-  });
-  setInterval(function () {
-    console.dir(image);
-  }, 16);
+(function d() {
+  setTimeout(d, 5);
+  debugger;
 })();
